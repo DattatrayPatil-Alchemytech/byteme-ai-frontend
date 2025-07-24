2140dfbe5a3c11aa6d810dbc43cd3db5efadab22
Dhara Popat <dhara@alchemytech.ca>
2025-07-24 14:14:41 +0530

    feat: BA:50 User history page created.

---
+++ app/(user)/history/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const UserHistoryPage = () => {
+  return (
+    <main>
+      <h1>User History</h1>
+      <p>This is the user history page. Filterable history and details will be shown here.</p>
+    </main>
+  );
+};
+
+export default UserHistoryPage; 
